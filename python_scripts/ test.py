a1aa9d2844267a6e0d504480a338cb696ff261d0
davisterno <davisterno@gmail.com>
2020-10-28 11:05:18 +0000

    testing

--- python_scripts/ test.py
+++ python_scripts/ test.py
@@ -1,15 +1,20 @@
 import keyboard
 import time
-while keyboard.KEY_DOWN:
-    if keyboard.is_pressed("up"):
-        print("moving forward")
-    if keyboard.is_pressed("down"):
-        print("moving back")
-    if keyboard.is_pressed("left"):
-        print("turning left")
-    if keyboard.is_pressed("right"):
-        print("turning right")
-    if keyboard.is_pressed("space + x"):
-        break
-"""else:
-    print("haulted")"""
+
+if selected_mode == ("control"):
+    while keyboard.KEY_DOWN:
+        if keyboard.is_pressed("up"):
+            print("moving forward")
+            #move_forward()
+        if keyboard.is_pressed("down"):
+            #move_backwards()
+            print("moving back")
+        if keyboard.is_pressed("left"):
+            #turn_left()
+            print("turning left")
+        if keyboard.is_pressed("right"):
+            #turn_right()
+            print("turning right")
+        if keyboard.is_pressed("ctrl + x"):
+            break
+    
