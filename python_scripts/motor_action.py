b9a6c5d1f23cbd62c397100339a451a069b2fc33
davisterno <davisterno@gmail.com>
2020-10-30 15:40:02 +0000

    improved the while loop

--- python_scripts/motor_action.py
+++ python_scripts/motor_action.py
@@ -22,51 +22,46 @@ GPIO.setwarnings(False)
 
 #IMPORTS
 from time import sleep   #this is an invaluable function for pausing the script
-
-import keyboard 
-import os                #should import functions from os, you never know when you might need it
-#import RPi.GPIO as GPIO  #this may not be necessary  #I dont think I will use this yet...
-os.system ("sudo pigiod")#launching the GPIO library - DOESN'T ALWAYS WORK
-sleep(2)                 #gives time for the system to process
+import keyboard          #gives time for the system to process
 import pigpio            #GPIO is identified on broadcom numbers
-
+print("*" * 10, "\n")
+print("""This script will not run unless the pigpio daemon has been activated by: 'sudo pigpiod'
+This script will also not run if it was not sudo called from the terminal: 'sudo python3 motor_control.py'""", "\n")
+print("*" * 10, "\n")
 
 #INITIAL SET-UP
-
 ESC_1 = 27  # Motor 1 control. this will need to be changed for the actual pin when plugged im
 ESC_2 = 13  # Motor 2 control. 'GPIO pin number' #Connect the ESC in a GPIO pin eg. 4
 
 pi = pigpio.pi()  #Initialise Pi connection
 
-pi.set_PWM_dutycycle(ESC_1,   0) # initial PWM off so as not to ruin anything
-pi.set_PWM_dutycycle(ESC_2,   0) # initial PWM off ""\
-
-""" Frequency of the PWM is defualt 8000 - 10 Hz 
-The frequency can be set by using:  pi.set_PWM_frequency(gpio, frequency_value)
-"""
-pi.set_PWM_frequency(ESC_1,50)  # set the frequency to a servo length standard
-pi.set_PWM_frequency(ESC_2,50)  # ""
-
-pi.set_mode(ESC_1, pigpio.OUTPUT) # ESC_1 gpio as output
-pi.set_mode(ESC_2, pigpio.OUTPUT) # ESC_2 gpio as output
-
-pi.set_PWM_range(ESC_1, 100)  #Sets the duty cycle range of the PWM to 0-100%  *wonder if possible to set both in one operation?
-pi.set_PWM_range(ESC_2, 100)  #Sets the duty cycle range of the PWM to 0-100%
+max_throttle = 1900 #Max throttle input
+half_throttle = max_throttle/2 #half throttle input
+min_throttle = 1100  #Min throttle input, essentially a neutral
+low_throttle = 1140
 
 
-max_duty = 100 #Duty cycle input
-half_duty = 50 #half duty cycle input
-min_duty = 0   #Min duty cycle input
+#ESC arming
+pi.set_servo_pulsewidth(ESC_1,0)
+pi.set_servo_pulsewidth(ESC_2,0)
+sleep(2)
+pi.set_servo_pulsewidth(ESC_1, max_throttle)
+pi.set_servo_pulsewidth(ESC_2, max_throttle)
+sleep(2)
+pi.set_servo_pulsewidth(ESC_1, min_throttle)
+pi.set_servo_pulsewidth(ESC_2, min_throttle)
+sleep(1)
 
+#PROGRAM START
 boot_msg = """Welcome to Robotug drive operation program. 
 
 Please ensure you have calibrated the ESCs before running this program!
 
-Please type 'confirm' to condirm you have calibrated the ESC.\n"""
+Please type 'confirm' to confirm you have calibrated the ESC.\n"""
 print(boot_msg) 
 
-confirm = input()  #asking for a confirmation input
-if confirm == ("confirm"):  #conditional statements
+cnfrm = input()  #asking for a confirmation input
+if cnfrm == ("confirm"):  #conditional statements
     print("Excellent\n")
     
 else:
@@ -90,8 +85,8 @@ def move_forward():  #forward movement function
     """
     funciton will rotate both motors at equal speed
     """
-    pi.set_PWM_dutycycle(ESC_1, 50)
-    pi.set_PWM_dutycycle(ESC_2, 50)
+    pi.set_servo_pulsewidth(ESC_1, low_throttle)
+    pi.set_servo_pulsewidth(ESC_2, low_throttle)
 
 
 def move_backwards():
@@ -107,16 +102,16 @@ def turn_left():
     """
     In order to turn left we must engage the right motor ESC_2 and not move the left motor ESC_1
     """
-    pi.set_PWM_dutycycle(ESC_1,0)
-    pi.set_PWM_dutycycle(ESC_2,100)
+    pi.set_servo_pulsewidth(ESC_1,min_throttle)
+    pi.set_servo_pulsewidth(ESC_2,low_throttle)
 
 
 def turn_right():
     """
     In order to turn right we must engage the left motor ESC_1 and not move the right motor ESC_2
     """
-    pi.set_PWM_dutycycle(ESC_1,100)
-    pi.set_PWM_dutycycle(ESC_2,0)
+    pi.set_servo_pulsewidth(ESC_1,low_throttle)
+    pi.set_servo_pulsewidth(ESC_2,min_throttle)
 
 
 def spin():
@@ -133,12 +128,12 @@ def pre_planned():
     as an example:
     """
     move_forward()
-    sleep(1)
+    sleep(2)
     turn_left()
-    sleep(1)
+    sleep(2)
     turn_right()
-    sleep(1)
-    move_backwards()
+    sleep(2)
+    move_forward()
     
 
 
@@ -146,8 +141,8 @@ def stop():
     """
     function will hault both the motors, bringing Robotug to a stop.
     """
-    pi.set_PWM_dutycycle(ESC_1,0)
-    pi.set_PWM_dutycycle(ESC_2,0)
+    pi.set_servo_pulsewidth(ESC_1,min_throttle)
+    pi.set_servo_pulsewidth(ESC_2,min_throttle)
 
 
 # ACUTUAL PROGRAMME
@@ -159,18 +154,21 @@ if selected_mode == ("control"):
         if keyboard.is_pressed("up"):
             print("moving forward")
             move_forward()
-        if keyboard.is_pressed("down"):
+        elif keyboard.is_pressed("down"):
             move_backwards()
             print("moving back")
-        if keyboard.is_pressed("left"):
+        elif keyboard.is_pressed("left"):
             turn_left()
             print("turning left")
-        if keyboard.is_pressed("right"):
+        elif keyboard.is_pressed("right"):
             turn_right()
             print("turning right")
-        if keyboard.is_pressed("ctrl + x"):
+        elif keyboard.is_pressed("ctrl + x"):
             break
-    
+        else:
+            stop()
+            print("stopped")
+            
 elif selected_mode == ("pre-planned"):
     pre_planned()
 
