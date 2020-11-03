6f68b1edf11491d527d2c3c7ef98e8b85058b667
davisterno <davisterno@gmail.com>
2020-11-03 14:56:25 +0000

    test relays

--- python_scripts/ test.py
+++ python_scripts/ test.py
@@ -1,20 +1,10 @@
 import keyboard
 import time
-selected_mode = input()  #asking for an input from keyboard command
+import os
+import pigpio
 
-if selected_mode == ("control"):
-    while keyboard.KEY_DOWN:
-        if keyboard.is_pressed("up"):
-            print("moving forward")
-            #move_forward()
-        if keyboard.is_pressed("down"):
-            #move_backwards()
-            print("moving back")
-        if keyboard.is_pressed("left"):
-            #turn_left()
-            print("turning left")
-        if keyboard.is_pressed("right"):
-            #turn_right()
-            print("turning right")
-        if keyboard.is_pressed("ctrl + x"):
-            break
+pi = pigpio.pi()
+gpio_list = (22,23)
+
+pi.write(gpio_list, True)
+
