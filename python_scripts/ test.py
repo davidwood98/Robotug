a8cd9b709ea2ed1101811e5dd31007797f972f7c
davisterno <davisterno@gmail.com>
2020-11-03 16:28:56 +0000

    test

--- python_scripts/ test.py
+++ python_scripts/ test.py
@@ -13,11 +13,19 @@ time.sleep(2)
 pi.set_servo_pulsewidth(ESC_1, 1900)
 time.sleep(2)
 pi.set_servo_pulsewidth(ESC_1, 1100)
-time.sleep(2)
+time.sleep(1)
 pi.set_servo_pulsewidth(ESC_1, 1150)
+time.sleep(5)
+pi.set_servo_pulsewidth(ESC_1, 0)
+time.sleep(2)
+pi.write(22, 0)
+pi.write(23, 0)
+time.sleep(1)
+pi.set_servo_pulsewidth(ESC_1, 1150)
+time.sleep(5)
+pi.set_servo_pulsewidth(ESC_1, 1100)
 
 
 
-
-
+pi.stop()
 
