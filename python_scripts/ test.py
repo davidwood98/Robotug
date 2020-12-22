326057c85eb5971bf0b27b17d2d3fc027504626c
davisterno <davisterno@gmail.com>
2020-12-22 15:14:21 +0000

    d

--- python_scripts/ test.py
+++ python_scripts/ test.py
@@ -141,14 +141,12 @@ def collision_detection():
             
             if len(scan) > 50:
                 for (quality, angle, distance) in scan:
-                    if angle in range(90, 170) and distance <= 505:
-                        print("collision detected")
-                        dual_motorstop()
-                        while angle in range(90, 170) and distance <= 505:
-                            collision_avoid()
 
-                        break
-                    
+                    while angle in range(90, 170) and distance <= 505:
+                        print("collision detected")
+                        collision_avoid()
+
+                        #break
                     else:
                         motor.move_forward(pi, ESC1, ESC2, relay_left_ch1, relay_left_ch2, relay_right_ch1, relay_right_ch2, low_throttle)  
                         #pass
