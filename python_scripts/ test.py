a85dd211d0cd218333a6f9119a23a454d412ce67
davisterno <davisterno@gmail.com>
2020-12-23 16:03:41 +0000

    d

--- python_scripts/ test.py
+++ python_scripts/ test.py
@@ -138,7 +138,8 @@ def collision_detection():
     #motor.move_forward(pi, ESC1, ESC2, relay_left_ch1, relay_left_ch2, relay_right_ch1, relay_right_ch2, low_throttle)
     try:      
         for scan in lidar.iter_scans():
-            
+            print("moving forward")
+            #motor.move_forward(pi, ESC1, ESC2, relay_left_ch1, relay_left_ch2, relay_right_ch1, relay_right_ch2, low_throttle)
             if len(scan) > 10:
                 for (quality, angle, distance) in scan:
 
@@ -146,11 +147,10 @@ def collision_detection():
                         print("collision detected")
                         #collision_avoid()
                         #time.sleep(0.5)
+                    else:
                         break
-                        
                     
-                    print("moving forward")
-                    #motor.move_forward(pi, ESC1, ESC2, relay_left_ch1, relay_left_ch2, relay_right_ch1, relay_right_ch2, low_throttle)
+                    
 
 
                    
