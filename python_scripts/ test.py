bc8682d5d98ca43d2712c1e1df1600f12a2e9d4a
davisterno <davisterno@gmail.com>
2020-12-27 16:14:42 +0000

    get angle

--- python_scripts/ test.py
+++ python_scripts/ test.py
@@ -137,18 +137,19 @@ def collision_detection():
     print("*" * 20)
     #motor.move_forward(pi, ESC1, ESC2, relay_left_ch1, relay_left_ch2, relay_right_ch1, relay_right_ch2, low_throttle)
     try:      
-        for scan in lidar.iter_scans():
+        for scan in lidar.iter_scans():   #scan is a list of tuples
             #print("moving forward")
-            if len(scan) > 100:
-                for (quality, angle, distance) in scan:
+            if len(scan) > 50:
+                for (quality, angle, distance) in scan:   # iterates for each tuple in the list scan
 
                     if angle in range(90, 170) and distance <= 505:
                         print("collision detected")
+                        print(angle)
                         #collision_avoid()
                         #time.sleep(1)
                         break
                     else:
-                        print("moving forward")
+                        #print("moving forward")
                         #motor.move_forward(pi, ESC1, ESC2, relay_left_ch1, relay_left_ch2, relay_right_ch1, relay_right_ch2, low_throttle) 
                         pass
                     
